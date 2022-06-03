06d0a880a3665148ece3fe01a598d46142d74001
David Kontyko <33853225+dkontyko@users.noreply.github.com>
2022-06-03 17:59:58 -0400

    adding XML to JSON

--- src/main/java/app/djk/RestPdfFormFiller/functions/HttpTriggerFunctions.java
+++ src/main/java/app/djk/RestPdfFormFiller/functions/HttpTriggerFunctions.java
@@ -1,6 +1,7 @@
 package app.djk.RestPdfFormFiller.functions;
 
 import app.djk.RestPdfFormFiller.Pdf.RestPdfApi;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
@@ -16,6 +17,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Base64;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.logging.Level;
 
@@ -76,17 +78,34 @@ public class HttpTriggerFunctions {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("No content supplied in body.").build();
         }
 
+        final var returnDataFormat = request.getQueryParameters().get("format");
+
+        if(!(Objects.equals(returnDataFormat, "json")) && !(Objects.equals(returnDataFormat, "xml"))) {
+            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Invalid format parameter: Must be 'json' or 'xml'.").build();
+        }
+
         // The function expects the PDF to be encoded in Base64 for safe transit over the internet.
         var requestBytes = Base64.getDecoder().decode(request.getBody());
         context.getLogger().info("Request length (number of bytes): " + requestBytes.length);
 
         try {
-            final var datasetsString = RestPdfApi.get4187DatasetNodeAsString(requestBytes);
+            var datasetsString = RestPdfApi.get4187DatasetNodeAsString(requestBytes);
+
+            if(returnDataFormat.equals("json")) {
+                datasetsString = RestPdfApi.convertXmlToJson(datasetsString);
+            }
             return request.createResponseBuilder(HttpStatus.OK).body(datasetsString).build();
+
+        } catch (JsonProcessingException e) {
+            // This is above IOException because JsonProcessingExceptions inherits from it.
+            context.getLogger().log(Level.SEVERE, e.toString());
+            return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("Converting XML to JSON failed.").build();
+
         } catch (IOException e) { // Catching errors from the RestPdfApi methods.
             context.getLogger().log(Level.SEVERE, "Unable to create InputStream.");
             context.getLogger().log(Level.SEVERE, e.toString());
             return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("Request failed.").build();
+
         } catch (TransformerException e) {
             context.getLogger().log(Level.SEVERE, e.toString());
             return request.createResponseBuilder(HttpStatus.INTERNAL_SERVER_ERROR).body("DOM Transform failed.").build();
